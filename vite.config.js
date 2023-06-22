e51ce2d83eb163e90de89e234247264129c910f0
Artur Barcicki <barcicki@gmail.com>
2023-06-23 00:07:00 +0200

    Added PWA support with initial icon

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['trio-32.svg', 'trio-512.svg'],
+      manifest: {
+        name: 'Trio',
+        short_name: 'Trio',
+        description: 'Matching tile game',
+        theme_color: '#242424',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: 'trio-512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': './src'
