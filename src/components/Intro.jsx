00a4aada51696fda30154331a647d9115ebfe642
Artur Barcicki <barcicki@gmail.com>
2023-06-24 14:17:44 +0200

    Added back on intro screens

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,9 @@
 import './Intro.css';
+import { useNavigate } from 'react-router-dom';
 
 export function Intro({ goal, children }) {
+  const navigate = useNavigate();
+
   return (
     <main className="intro">
       <div className="intro-goal">
@@ -8,6 +11,9 @@ export function Intro({ goal, children }) {
       </div>
       <div className="intro-options">
         {children}
+      </div>
+      <div className="intro-footer">
+        <button onClick={() => navigate(-1)}>Back</button>
       </div>
     </main>
   );
