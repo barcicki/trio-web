fb2e7d7f644aec43361f983b8310737aac540558
Artur Barcicki <barcicki@gmail.com>
2023-11-03 18:50:58 +0100

    Minor shapes theme look improvements

--- src/components/TileThemes/ShapeTileContent.jsx
+++ src/components/TileThemes/ShapeTileContent.jsx
@@ -52,28 +52,28 @@ export function ShapeTileContent({ tile }) {
       content.push({
         x: 100,
         y: 100,
-        size: 100
+        size: 80
       });
       break;
     case 1:
       content.push({
-        x: 60,
-        y: 60,
-        size: 65
+        x: 65,
+        y: 65,
+        size: 60
       }, {
-        x: 140,
+        x: 135,
         y: 140,
-        size: 65
+        size: 60
       });
       break;
     case 2:
       content.push({
-        x: 55,
-        y: 60,
+        x: 60,
+        y: 65,
         size: 60
       }, {
-        x: 145,
-        y: 60,
+        x: 140,
+        y: 65,
         size: 60
       }, {
         x: 100,
@@ -89,7 +89,7 @@ export function ShapeTileContent({ tile }) {
         <line x1="0" y="0" x2="0" y2="1" stroke={props.stroke} strokeWidth=".2" />
       </pattern>)}
 
-      <rect className="tile-bg" fill="#fff" stroke="currentColor" strokeWidth="5" rx="20" ry="20" x="5" y="5" width="190" height="190"></rect>
+      <rect className="tile-bg" fill="#fff" stroke="currentColor" strokeWidth="5" rx="20" ry="20" x="10" y="10" width="180" height="180"></rect>
       {content.map((size, index) => (<Renderer key={index} {...size} {...props} />))}
     </>
   );
